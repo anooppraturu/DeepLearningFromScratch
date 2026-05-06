147134819db8696c9184ac60055c106ae586fc4c
anooppraturu <anooppraturu@gmail.com>
2026-05-06 15:53:49 -0700

    added adam optimizer

--- optimizers.py
+++ optimizers.py
@@ -33,7 +33,14 @@ class NoWeightDecay:
 
 
 class SGDOptimizer:
-    def __init__(self, model, lr0=0.1, alpha=1.0, tau=10000, weight_decay = 0.0, reg_type = 'L2'):
+    def __init__(self,
+                  model, 
+                  lr0=0.1, 
+                  alpha=1.0, 
+                  tau=10000, 
+                  weight_decay = 0.0, 
+                  reg_type = 'L2'
+                  ):
         # register parameters
         self.params = model.parameters()
 
@@ -56,7 +63,62 @@ class SGDOptimizer:
         lr = self.lr0 / np.power(1 + self.t/self.tau, self.alpha)
 
         for p in self.params:
+            param = p['value']
+            grad = p['grad']
             if p['weight_decay']:
-                p['value'] -= lr * (p['grad'] + self.reg.grad(p['value']))
+                param -= lr * (grad + self.reg.grad(param))
             else:
-                p['value'] -= lr * p['grad']
+                param -= lr * grad
+
+
+class AdamOptimizer:
+    def __init__(self,
+                  model, 
+                  b1=0.9, 
+                  b2=0.999,
+                  lr0=0.1,
+                  alpha=1.0, 
+                  tau=10000, 
+                  weight_decay = 0.0, 
+                  reg_type = 'L2'
+                  ):
+        # register parameters
+        self.params = model.parameters()
+
+        # Weight Decay
+        if reg_type == 'L2':
+            self.reg = L2Regularizer(weight_decay)
+        elif reg_type == 'L1':
+            self.reg = L1Regularizer(weight_decay)
+        elif reg_type == 'None':
+            self.reg = NoWeightDecay()
+
+        #scheduler hyperparams
+        self.lr0 = lr0
+        self.alpha = alpha
+        self.tau = tau
+        self.t = 0
+
+        #Adam hyperparameters and gradient buffers
+        self.b1 = b1
+        self.b2 = b2
+        self.eps = 1e-8
+        self.mt = [np.zeros_like(p['value']) for p in self.params]
+        self.vt = [np.zeros_like(p['value']) for p in self.params]
+
+    def step(self):
+        self.t += 1
+        bias_correction = np.sqrt(1 - np.power(self.b2, self.t))/(1 - np.power(self.b1, self.t))
+        lr = bias_correction * self.lr0 / np.power(1 + self.t/self.tau, self.alpha)
+
+        for i, p in enumerate(self.params):
+            param = p['value']
+            grad = p['grad']
+
+            self.mt[i] = self.b1*self.mt[i] + (1 - self.b1)*grad
+            self.vt[i] = self.b2*self.vt[i] + (1 - self.b2)*np.square(grad)
+
+            if p['weight_decay']:
+                param -= lr*(self.mt[i] / (np.sqrt(self.vt[i]) + self.eps) + self.reg.grad(param))
+            else:
+                param -= lr * self.mt[i] / (np.sqrt(self.vt[i]) + self.eps)
