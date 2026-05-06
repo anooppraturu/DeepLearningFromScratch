91c802d2160e5743004a563d3c3fd20bb569fe5c
anooppraturu <anooppraturu@gmail.com>
2026-05-06 15:27:30 -0700

    added weight decay to SGD optimizer

--- optimizers.py
+++ optimizers.py
@@ -1,10 +1,49 @@
 import numpy as np
 from model import Sequential
 
+class L2Regularizer:
+    def __init__(self, lam):
+        self.lam = lam
+
+    def penalty(self, params):
+        return self.lam*0.5*np.sum(np.square(params))
+    
+    def grad(self, params):
+        return self.lam*params
+    
+class L1Regularizer:
+    def __init__(self, lam):
+        self.lam = lam
+
+    def penalty(self, params):
+        return self.lam*np.sum(np.abs(params))
+    
+    def grad(self, params):
+        return self.lam*np.sign(params)
+    
+class NoWeightDecay:
+    def __init__(self, lam=0.0):
+        self.lam = lam
+
+    def penalty(self, params):
+        return 0.0
+    
+    def grad(self, params):
+        return 0.0
+
+
 class SGDOptimizer:
-    def __init__(self, model, lr0=0.1, alpha=1.0, tau=10000):
+    def __init__(self, model, lr0=0.1, alpha=1.0, tau=10000, weight_decay = 0.0, reg_type = 'L2'):
         # register parameters
         self.params = model.parameters()
+
+        # Weight Decay
+        if reg_type == 'L2':
+            self.reg = L2Regularizer(weight_decay)
+        elif reg_type == 'L1':
+            self.reg = L1Regularizer(weight_decay)
+        elif reg_type == 'None':
+            self.reg = NoWeightDecay()
 
         #scheduler hyperparams
         self.lr0 = lr0
@@ -17,4 +56,7 @@ class SGDOptimizer:
         lr = self.lr0 / np.power(1 + self.t/self.tau, self.alpha)
 
         for p in self.params:
-            p['value'] -= lr*p['grad']
+            if p['weight_decay']:
+                p['value'] -= lr * (p['grad'] + self.reg.grad(p['value']))
+            else:
+                p['value'] -= lr * p['grad']
