da0e4b9554d3457bd4417a9099f6cd53d5cd1953
anooppraturu <anooppraturu@gmail.com>
2026-05-19 22:09:10 -0700

    update train function to take dataloaders, cleaned up loss logging

--- src/shasta/training.py
+++ src/shasta/training.py
@@ -1,31 +1,38 @@
 import numpy as np
 
-def train_model(model, optimizer, loss_fn, x, y, epochs, batch_size, verbose=False):
-    loss_history = []
-    N = x.shape[0]
+def train_model(model, optimizer, loss_fn, TrainLoader, ValidationLoader=None, epochs=5):
+    mean_train_loss = []
+    validation_loss = []
     cadence = max(epochs // 10, 1)
 
     for epoch in range(epochs):
-        shuffle_order = np.random.permutation(N)
-        for i in range(0, N, batch_size):
-            batch = shuffle_order[i: i+batch_size]
+        epoch_losses = []
 
-            preds = model.forward(x[batch])
-            loss = loss_fn.forward(preds, y[batch])
+        model.train()
+        for xb, yb in TrainLoader:
+            preds = model.forward(xb)
+            loss = loss_fn.forward(preds, yb)
 
             grad = loss_fn.backward()
             grad = model.backward(grad)
 
             optimizer.step()
 
-            if verbose:
-                loss_history.append(loss)
+            epoch_losses.append(loss)
 
-        loss_history.append(loss)
+        mean_train_loss.append(np.mean(epoch_losses))
+
+        if ValidationLoader is not None:
+            val_losses = []
+            for xvb, yvb in ValidationLoader:
+                preds = model.forward(xvb)
+                loss = loss_fn.forward(preds, yvb)
+                val_losses.append(loss)
+            validation_loss.append(np.mean(val_losses))
         
         if epoch % cadence == 0:
-            print(f"epoch={epoch}, loss={loss_history[-1]}")
+            print(f"epoch={epoch}, train loss={mean_train_loss[-1]}, validation loss={validation_loss[-1]}")
 
-    return loss_history
+    return mean_train_loss, validation_loss
 
         
